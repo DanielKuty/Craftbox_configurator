0f03c849b2365acedb5cda0ad4f6037a7f266123
DanielKuty <daniel.kuty29@gmail.com>
2017-12-16 05:34:02 -0500

    Button add

--- src/craftbox_configurator/main.py
+++ src/craftbox_configurator/main.py
@@ -2,21 +2,24 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
-
-class LoginScreen(GridLayout):
+class LoginScreen(GridLayout, Button):
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
+        self.cols = 4
+        self.add_widget(Label(text='SSID'))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
         self.add_widget(Label(text='password'))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
-
-
+        self.add_widget(Label(text="PSK"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.button = Button(text="Konfigure", font_size=15, multiline=False)
+        self.add_widget(self.button)
 class MyApp(App):
 
     def build(self):
@@ -25,3 +28,6 @@ class MyApp(App):
 
 if __name__ == '__main__':
     MyApp().run()
+
+
+
